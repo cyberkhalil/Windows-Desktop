7972791889840af7c3ecc5bcd23772243490d10b
Mahmoud khalil <khalil2535@protonmail.com>
2019-11-09 00:08:44 +0200

    split main from utilities

--- setup.ps1
+++ setup.ps1
@@ -1,20 +1,29 @@
 # install choco
 iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
-# main things and utilities
-choco install 7zip ccleaner choco-cleaner winrar lightshot firefox chocolatey-windowsupdate.extension Everything fluent-terminal fscapture netcat notepadplusplus Opera PowerShell teamviewer
+# Main things
+choco install 7zip ccleaner choco-cleaner winrar lightshot firefox chocolatey-windowsupdate.extension Everything fscapture notepadplusplus PowerShell -y
+
+# Utilities
+choco install teamviewer Opera fluent-terminal netcat vlc -y
+
+# Documents stuff
+#choco install foxitreader -y
 
 # Analysis & Debug tools
-# choco install Strings dependencywalker fakenet hashdeep ida-free ollydbg procexp RegShot reshack upx x64dbg
+# choco install Strings dependencywalker fakenet hashdeep ida-free ollydbg procexp procmon RegShot reshack upx x64dbg -y
 
 # Windows Administration
-# choco install rsat sysinternals
+# choco install rsat sysinternals -y
 
 # C# Development tools
-# choco install git dotnetfx visualstudio2019community postman visualstudio-installer
+# choco install git dotnetfx visualstudio2019community postman visualstudio-installer -y
 
 # Java Development
-# choco install git intellijidea-community jdk8
+# choco install jdk8 git intellijidea-community netbeans -y
 
 # Python Development
-# choco install python python3
+# choco install python python3 -y
+
+# PHP Development
+#choco install php -y
